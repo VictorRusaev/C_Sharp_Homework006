d1685beb680e69e6f2627b43cb55fe79c948a0fb
Victor Rusaev <rusaev74@mail.ru>
2022-10-07 18:08:50 +0300

    TASK002 point of crossing

---
+++ TASK002_point_of_crossing/Program.cs
@@ -0,0 +1,19 @@
+﻿//Точка пересечения прямых
+Console.Clear();
+
+Console.Write("Введите b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine()!);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения имеет координаты: ({x}; {y})");
