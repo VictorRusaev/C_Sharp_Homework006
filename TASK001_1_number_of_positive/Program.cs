a37e1799a15d7cbb14fc5af5873377858f003faa
Victor Rusaev <rusaev74@mail.ru>
2022-10-07 17:50:42 +0300

    TASK001_2 with recursion

---
+++ TASK001_1_number_of_positive/Program.cs
@@ -0,0 +1,37 @@
+﻿// Нерекурсивное решение.
+Console.Clear();
+
+Console.Write("Введите количество чисел: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] array = CreateArray(number);
+FillArray(array);
+CountPositive(array);
+
+Console.WriteLine($"Положительных: {CountPositive(array)}");
+
+int[] CreateArray(int num)
+{
+    int[] arr = new int[num];
+    return arr;
+}
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+int CountPositive(int[] array)
+{
+    int counter = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
---
+++ TASK001_2_positive_with_recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿// Рекурсивное решение.
+Console.Clear();
+
+Console.Write("Введите количество чисел: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] array = CreateArray(number);
+FillArray(array, number);
+Console.WriteLine($"Положительных: {CountPositive(array)}");
+
+int[] CreateArray(int num)
+{
+    int[] arr = new int[num];
+    return arr;
+}
+
+int[] FillArray(int[] array, int number, int i = 0)
+{
+    if(i < number)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+        i++;
+        FillArray(array, number, i);
+    }
+    return array;
+}
+
+int CountPositive(int[] array)
+{
+    int counter = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
